1707236ebe81b4e5b7c6fa15c602c9746437b988
fireballreco <69571329+fireballreco@users.noreply.github.com>
2020-08-25 00:20:24 -0500

    initial xcode project creation

---
+++ myfirstonedaybuild/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  myfirstonedaybuild
+//
+//  Created by Fireball Reco on 8/25/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
