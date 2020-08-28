bdd976aa1b25e8404519e69d4ccfae6988f9118c
fireballreco <69571329+fireballreco@users.noreply.github.com>
2020-08-28 12:55:59 -0500

    Added video model and networing code to connect with YouTube Data API

---
+++ myfirstonedaybuild/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  myfirstonedaybuild
+//
+//  Created by Fireball Reco on 8/26/20.
+//
+
+import Foundation
---
+++ myfirstonedaybuild/Video.swift
@@ -0,0 +1,8 @@
+//
+//  Video.swift
+//  myfirstonedaybuild
+//
+//  Created by Fireball Reco on 8/26/20.
+//
+
+import Foundation
