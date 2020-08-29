e9db21c1585e8ade7e040cd25d57f2e0e0751484
fireballreco <69571329+fireballreco@users.noreply.github.com>
2020-08-28 19:38:21 -0500

    Added code to parse the json response

--- myfirstonedaybuild/Model.swift
+++ myfirstonedaybuild/Model.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+class Model {
+    
+    func getVideos() {
+        
+        // Create a URL object
+        let url = URL(string: Constants.API_URL)
+        
+        guard url != nil else{
+            return
+        }
+        
+        // Get a urlsession object
+        let session = URLSession.shared
+        
+        // Get a data task from the URLSession object
+        let dataTask = session.dataTask(with: url!) { (data, response, error) in
+            
+            // Check if there were any errors
+            if error != nil || data == nil {
+                return
+            }
+            
+            do {
+                // Parsing the data into video objects
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .iso8601
+                
+                let response = try decoder.decode(Response.self, from: data!)
+                
+                dump(response)
+            }
+            
+            catch {
+                
+            }
+            
+        }
+        // Kick off the task
+        dataTask.resume()
+    }
+}
--- myfirstonedaybuild/ViewController.swift
+++ myfirstonedaybuild/ViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var model = Model()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        model.getVideos()
         
-        var i = 0
     }
 
 
